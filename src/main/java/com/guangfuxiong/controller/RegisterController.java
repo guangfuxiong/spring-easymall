e283d65f04d33b2b17d593b599ee46ddda804bc8
guangfuxiong <guangfuxiong@163.com>
2019-01-10 20:04:09 +0800

    springMVC中利用response传递响应头

--- src/main/java/com/guangfuxiong/controller/RegisterController.java
+++ src/main/java/com/guangfuxiong/controller/RegisterController.java
@@ -8,19 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class RegisterController {
 
     @Autowired
     private RegisterService registerService;
     @RequestMapping(value = "register")
-    public String register(UserInfo userInfo, Model model){
+    public String register(UserInfo userInfo, Model model, HttpServletResponse response) throws IOException {
         Integer num = registerService.addUser(userInfo);
         ResultRet resultRet = new ResultRet();
         if(num>=1){
             resultRet.setCode(1);
             resultRet.setMessage("注册成功，3秒后返回登录页面");
             model.addAttribute("result",resultRet);
+//            response.setContentType("text/html;charset=utf-8");
+//            response.getWriter().write("注册成功，3秒后返回登录页面-response");
+//            response.setHeader("refresh","3;url=toPage/toLogin.do");
             return "registerSuccessTip";
         }
         resultRet.setCode(0);
